e3e4676928bbc855f2085b13340459bdc629bf39
Soham Sen <hello@sohamsen.me>
2023-02-04 02:57:22 +0530

    Added insight

--- packages/game-snap/src/index.ts
+++ packages/game-snap/src/index.ts
@@ -1,80 +1,22 @@
-import { OnRpcRequestHandler } from '@metamask/snap-types';
-import { ethers } from 'ethers';
+import {
+  OnRpcRequestHandler,
+  OnTransactionHandler,
+} from '@metamask/snap-types';
+import { decodeTransferData, mintNFT, transferNFT } from './utils';
 
-const CONTRACT_ADDRESS = '0xB967F2C084617c83C0618F35Be9970e0d571137f';
-
-const mintNFT = async (tokenURI: string) => {
-  let simpleNFTInterface = new ethers.utils.Interface([
-    'constructor(string memory name_, string memory symbol_)',
-    'function mint(string memory tokenURI) public returns (uint256)',
-    'function transferFrom(address from, address to, uint256 tokenId)',
-  ]);
-  let functionData = simpleNFTInterface.encodeFunctionData('mint', [tokenURI]);
-  try {
-    const [from] = (await wallet.request({
-      method: 'eth_requestAccounts',
-    })) as string[];
-    // Send a transaction to MetaMask.
-    const data = await wallet.request({
-      method: 'eth_sendTransaction',
-      params: [
-        {
-          from,
-          to: CONTRACT_ADDRESS,
-          value: '0x0',
-          data: functionData,
-        },
-      ],
-    });
-    console.log(data);
-    return data;
-  } catch (e) {
-    console.error(e);
-  }
-  return false;
-};
-
-const transferNFT = async (toAddress: string, tokenID: number) => {
-  const simpleNFTInterface = new ethers.utils.Interface([
-    'constructor(string memory name_, string memory symbol_)',
-    'function mint(string memory tokenURI) public returns (uint256)',
-    'function transferFrom(address from, address to, uint256 tokenId)',
-  ]);
-  try {
-    const [from] = (await wallet.request({
-      method: 'eth_requestAccounts',
-    })) as string[];
-    const functionData = simpleNFTInterface.encodeFunctionData('transferFrom', [
-      from,
-      toAddress,
-      tokenID,
-    ]);
-    const confirmResult = await wallet.request({
-      method: 'snap_confirm',
-      params: [
-        {
-          prompt: 'Would you like to transfer the NFT?',
-          description: `You will be transfering this NFT (${tokenID}) to ${toAddress}`,
-        },
-      ],
-    });
-    if (!confirmResult) return false;
-    // Send a transaction to MetaMask.
-    const data = await wallet.request({
-      method: 'eth_sendTransaction',
-      params: [
-        {
-          from,
-          to: CONTRACT_ADDRESS,
-          value: '0x0',
-          data: functionData,
-        },
-      ],
-    });
-    console.log(data);
-  } catch (e) {
-    console.error(e);
-  }
+export const onTransaction: OnTransactionHandler = async ({
+  transaction,
+  chainId,
+}) => {
+  console.log(transaction);
+  if (!transaction.data) return { insights: {} };
+  console.log(transaction.data);
+  const data = decodeTransferData(transaction.data as any);
+  return {
+    insights: {
+      'Transfering NFT': `You will be transfering your NFT (${data.tokenID}) to ${data.to}`,
+    },
+  };
 };
 
 /**
